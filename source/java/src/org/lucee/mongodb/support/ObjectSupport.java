c9af7fda279afa68b4e14d56667d17e84262ce7a
Jonathan van Zuijlekom <jvz@bridgevest.com>
2015-08-17 17:51:36 +0200

    Returns BasicDBObject

--- source/java/src/org/lucee/mongodb/support/ObjectSupport.java
+++ source/java/src/org/lucee/mongodb/support/ObjectSupport.java
@@ -236,7 +236,7 @@ public class ObjectSupport {
 		return obj;
 	}
 
-	public BasicDBObject toMongo(Map map) {
+	public Map toMongo(Map map) {
 		// single record in Map
 		if(map.size()==1) {
 			Entry e=(Entry) map.entrySet().iterator().next();
